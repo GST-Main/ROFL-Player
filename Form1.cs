22824327d5d7d39f4c09656718649ab73e41cb46
성택 권 <kkst9602@naver.com>
2022-11-18 20:35:25 +0900

    Update Langugage Combo Box

--- Form1.cs
+++ Form1.cs
@@ -1,13 +1,20 @@
+using System.Diagnostics;
+using System.Linq;
+using System.Text.RegularExpressions;
+
 namespace ROFL_Player
 {
 	public partial class Form1 : Form
 	{
-		(string, string)[] languages = new[] { ("English", "en_US"), ("English(UK)", "en_GB"), ("English(Australia)", "en_AU"), ("Korean", "ko_KR"), ("Chinese", "zh_CN"), ("Japanese", "ja_JP"), ("German", "de_DE"), ("Italian", "it_IT"), ("French", "fr_FR"), ("Spanish", "es_ES"), ("Spanish(Latin America)", "es_MX"), ("Russian", "ru_RU"), ("Greek", "el_GR"), ("Polish", "pl_PL"), ("Romanian", "ro_RO"), ("Portuguese", "pt_BR"), ("Taiwanese", "zh_TW"), ("Hungarian", "hu_HU"), ("Turkish", "tr_TR") };
+		(string language, string code)[] languages = new[] { ("English", "en_US"), ("English(UK)", "en_GB"), ("English(Australia)", "en_AU"), ("Korean", "ko_KR"), ("Chinese", "zh_CN"), ("Japanese", "ja_JP"), ("German", "de_DE"), ("Italian", "it_IT"), ("French", "fr_FR"), ("Spanish", "es_ES"), ("Spanish(Latin America)", "es_MX"), ("Russian", "ru_RU"), ("Greek", "el_GR"), ("Polish", "pl_PL"), ("Romanian", "ro_RO"), ("Portuguese", "pt_BR"), ("Taiwanese", "zh_TW"), ("Hungarian", "hu_HU"), ("Turkish", "tr_TR") };
+		(string language, string code)[]? availableLangs;
+		
+		string? exePath;
+		string? roflPath;
 
 		public Form1()
 		{
 			InitializeComponent();
-			InitLangComboItems();
 		}
 
 		private void button_FindEXE_Click(object sender, EventArgs e)
@@ -21,7 +28,11 @@ namespace ROFL_Player
 			var result = openFileDialog.ShowDialog();
 			if (result == DialogResult.OK)
 			{
-				textBox_exe.Text = openFileDialog.FileName;
+				exePath = openFileDialog.FileName;
+				textBox_exe.Text = exePath;
+				UpdateAvailableLanguages(Path.GetDirectoryName(exePath));
+				if (availableLangs != null)
+					UpdateLangComboItems();
 			}
 		}
 
@@ -40,12 +51,32 @@ namespace ROFL_Player
 			}
 		}
 
-		private void InitLangComboItems()
+		private void UpdateLangComboItems()
 		{
-			foreach ((string langauge, string code) elem in languages)
+			comboBox_Language.Items.Clear();
+			foreach ((string langauge, string code) elem in availableLangs)
 			{
 				comboBox_Language.Items.Add(elem.langauge);
 			}
+		}
+
+		private void UpdateAvailableLanguages(string exeDirectory)
+		{
+			string dataDirectory = Path.Combine(exeDirectory, "DATA", "FINAL");
+			string[] filePaths = Directory.GetFiles(dataDirectory);
+			string[] files = filePaths.Select(elem => Path.GetFileName(elem)).ToArray();
+			List<string> codes = new();
+			foreach (string file in files)
+			{
+				if (Regex.IsMatch(file, @"(UI\.).....(\.wad\.client)"))
+				{
+					var splitted = file.Split(".");
+					codes.Add(splitted[1]);
+				}
+			}
+
+			(string, string)[] available = Array.FindAll(languages, language => codes.Contains(language.code));
+			availableLangs = available;
 		}
 	}
 }
