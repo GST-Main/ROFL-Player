c5198219219a23ed81e6be260e508e451c665cb1
성택 권 <kkst9602@naver.com>
2022-11-18 17:48:24 +0900

    Add Languages

--- Form1.cs
+++ Form1.cs
@@ -2,7 +2,7 @@ namespace ROFL_Player
 {
 	public partial class Form1 : Form
 	{
-		(string, string)[] languages = new[] { ("English", "en_US"), ("Korean", "ko_KR") };
+		(string, string)[] languages = new[] { ("English", "en_US"), ("English(UK)", "en_GB"), ("English(Australia)", "en_AU"), ("Korean", "ko_KR"), ("Chinese", "zh_CN"), ("Japanese", "ja_JP"), ("German", "de_DE"), ("Italian", "it_IT"), ("French", "fr_FR"), ("Spanish", "es_ES"), ("Spanish(Latin America)", "es_MX"), ("Russian", "ru_RU"), ("Greek", "el_GR"), ("Polish", "pl_PL"), ("Romanian", "ro_RO"), ("Portuguese", "pt_BR"), ("Taiwanese", "zh_TW"), ("Hungarian", "hu_HU"), ("Turkish", "tr_TR") };
 
 		public Form1()
 		{
